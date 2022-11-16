962c663e07c315151ed87178685bb21f92164cdf
priyanka <priyanka.kumar@ametek.com>
2022-11-16 18:48:06 -0500

    new changes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2fff1c8fc9828486187db19d5c22d225d1a4f0a0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2fff1c8fc9828486187db19d5c22d225d1a4f0a0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twilio/sdk' => array(
+            'pretty_version' => '6.42.1',
+            'version' => '6.42.1.0',
+            'reference' => '8a1a5bc1616c4b50a23e09880c1e3d02f93a68ec',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twilio/sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
